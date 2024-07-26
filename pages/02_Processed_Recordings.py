99bfbf41b05577db9eea9b923c277a0e474c0c8a
Michal@MacBook <michal.marusan@gmail.com>
2024-07-26 14:54:18 +0200

    update

--- pages/02_Processed_Recordings.py
+++ pages/02_Processed_Recordings.py
@@ -16,8 +16,11 @@ if "recording" not in st.session_state:
     st.session_state.recording = None
 if "selected_recording" not in st.session_state:
     st.session_state.selected_recording = None
+if "transcript" not in st.session_state:
+    st.session_state.transcript = None
 
 container_name = "cc-stage2-transcript-analysis"
+container_name_transcript = "cc-stage1-transcript"
 # load the processed recordings from the Azure Blob Storage
 def load_recordings_from_blob(container_name):
     from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
@@ -49,7 +52,10 @@ def invoke_function(recording_name):
     # st.write(f"Function invoked with recording: {recording_name}")
     st.session_state.selected_recording = recording_name
     blob_stream = download_blob(get_blob_by_name(container_name, recording_name))
+    blod_stream_transcript = download_blob(get_blob_by_name(container_name_transcript, recording_name))
     st.session_state.recording = blob_stream
+    # st.session_state.transcript = blod_stream_transcript.replace("\n", "\n\n")
+    st.session_state.transcript = blod_stream_transcript
 
 
 def display_recordings(recordings):
@@ -58,8 +64,50 @@ def display_recordings(recordings):
         with col2:
             st.write(recording_name)
         with col3:
-            if st.button(f"show details"):
+            if st.button(f"show details", key=recording_name):
                 invoke_function(recording_name)
+
+import re
+def parse_line(line):
+    # Define the regular expression pattern
+    pattern = r'\[(.*?)\] (.*?):\s*(.*)'
+    
+    # Use re.match to parse the line
+    match = re.match(pattern, line)
+    
+    if match:
+        timestamp = match.group(1)
+        persona = match.group(2)
+        text = match.group(3)
+        return timestamp, persona, text
+    else:
+        raise ValueError("Line format is incorrect")
+
+
+def display_transcript(transcript):
+    # Split the transcript into lines
+    lines = transcript.split('\n')
+    
+    # Iterate over each line in the transcript
+    for line in lines:
+        timestamp, persona, text = parse_line(line)
+        if "agent" in persona:
+            # Display agent text as a chat message
+            with st.chat_message("assistant"):
+                st.write(text)
+                st.caption(f"{timestamp}[ms]")
+        elif "customer" in persona:
+            # Display customer text as a chat message
+            with st.chat_message("user"):
+                st.write(text)
+                st.caption(f"{timestamp}[ms]")
+        else:
+            pass
+
+if st.session_state.recordings is None:
+    st.session_state.recordings = load_recordings_from_blob(container_name)
+else:
+    st.info(f"Recordings already loaded.")
 
 if st.session_state.recordings is None:
     st.session_state.recordings = load_recordings_from_blob(container_name)
@@ -71,4 +119,8 @@ display_recordings(st.session_state.recordings)
 
 if st.session_state.recording is not None:
     with st.container(border=True):
+        st.header("Semantic Analysis Report")
         st.write(st.session_state.recording)
+    with st.container(border=True):
+        st.header("Transcript")
+        display_transcript(st.session_state.transcript)   
